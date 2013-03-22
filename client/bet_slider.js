343655c1ecb240783b770edad590c08b49c37046
KKudryavtsev <konstantin.k.ed@gmail.com>
2013-03-22 12:33:41 +0000

    betting interface now working

--- client/bet_slider.js
+++ client/bet_slider.js
@@ -1,6 +1,27 @@
 Meteor.startup(function(){
-  BetSlider = $("#bet-slider").rangeSlider({
+  var $betSlider = $("#bet-slider").rangeSlider({
     bounds:{min:1, max: 100},
-    wheelMode: "zoom"
-  });
-})
+    wheelMode: "zoom",
+    step: 1
+  }).on("valuesChanging", saveValues);
+
+  function saveValues(e, data){
+    Session.set("bet_slider", $.extend(Session.get("bet_slider"),{
+      values: data ? data.values : $betSlider.rangeSlider("values")
+    }));
+  }
+
+  saveValues();
+});
+
+BetSlider = {};
+BetSlider.getSliderVals = function() {
+  var bet_slider = Session.get("bet_slider");
+  
+  if(bet_slider && bet_slider.values){
+    return bet_slider.values;
+  }
+  
+  return undefined;
+}
+
--- client/templates/bet_inputs_area.js
+++ client/templates/bet_inputs_area.js
@@ -1,25 +1,54 @@
-_.extend(Template.bet_inputs_area,{
-  created: function(){
-    Session.set("current_stake", 0);
-  },
-  rendered: function(){
-    $("#bet-inputs-area .numeric-input").numeric();
-  }
-});
+(function(){
+  _.extend(Template.bet_inputs_area,{
+    created: function(){
+      Session.set("current_stake", 0);
+    },
+    rendered: function(){
+      $("#bet-inputs-area .numeric-input").numeric();
+      $("#bet-inputs-stake").focus(function(){
+        $("#bet-inputs-stake").select().one("mouseup", function(e){
+          e.preventDefault();
+          $(this).unbind("mouseup");
+        });
+      });
+    }
+  });
 
-Template.bet_inputs_area.helpers({
-  current_stake: function(){
-    return Session.get("current_stake");
-  },
-  range_size: function(){
-    return "CODE ME";
-  },
-  chance_to_win: function(){
-    return "CODE ME"
-  },
-  reward: function(){
-    return "CODE ME";
+  Template.bet_inputs_area.helpers({
+    current_stake: function(){
+      return 0;
+    },
+    range_size: function(){
+      return getRangeSize();
+    },
+    chance_to_win: function(){
+      return Math.round(getChanceToWin() * 10000) / 100 + "%";
+    },
+    reward: function(){
+      return Math.round(Session.get("current_stake") * (1 / getChanceToWin()) * 100) / 100;
+    }
+  });
+
+  Template.bet_inputs_area.events({
+    "keyup #bet-inputs-stake": function(){
+      Session.set("current_stake", parseFloat($("#bet-inputs-stake").val(),10));
+    }
+  });
+
+  function getRangeSize(){
+    var vals = BetSlider.getSliderVals();
+    return vals ? vals.max - vals.min + 1 : 0;
   }
-});
+
+  function getChanceToWin(){
+    var vals = BetSlider.getSliderVals();
+
+    if(vals){
+      return getRangeSize() / 101
+    }else{
+      return 0;
+    }
+  }
+})();
 
 
