8726c6bb6df7ac6f60790c16c6d20052526ed2a0
KKudryavtsev <konstantin.k.ed@gmail.com>
2013-03-24 18:54:07 +0000

    wrote and tested memorise

--- client/templates/lib/memorise.js
+++ client/templates/lib/memorise.js
@@ -1,3 +1,15 @@
-memorise = function(funcs){
+templateHelpers.memorise = function(funcs){
+  var wrapped = {};
 
+  _.each(funcs, function(val, key){
+    var last_value;
+    
+    wrapped[key] = function(){
+      last_value = val.apply(this, [last_value].concat(Array.prototype.slice.call(arguments)));
+      return last_value;
+    }
+  });
+
+  return wrapped;
 }
+
