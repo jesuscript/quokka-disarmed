653bbd06c75d5d4a7f8c6bba8a08451c81fac6aa
Stephan Tual <stephan@r4dical.com>
2013-04-20 17:58:46 +0100

    test - ignore

--- server/main.js
+++ server/main.js
@@ -3,7 +3,7 @@ __meteor_bootstrap__.app
   .use(connect.query())
   .use(function(req, res, next) {
     // check if the url path is a valid sha256 hash 
-    if (req.url.search(/^\/[a-fA-F0-9]{64}$/) == 0) {
+    if (req.url.length == 65 && req.url.substr(1,64).search(/[a-fA-F0-9]{64}$/) == 0) {
       // console.log('valid hash '+ req.url.substr(1,64) +  'found in URL, moving on');
       // if btc account exists
       //var account = Accounts.find({hash: req.url.substr(1,64)}, {fields: {address: 1}}); // TODO: is this async?
@@ -17,7 +17,8 @@ __meteor_bootstrap__.app
       crypto.randomBytes(512, function(ex, buf) {
         if (ex) throw ex;
         var hash = crypto.createHash('sha256').update(buf).digest("hex");
-        res.writeHead(301, {'Location': hash});
+        console.log(req.headers.host + '/' + hash);
+        res.writeHead(301, {'Location': req.headers['x-forwarded-proto'] + '://' + req.headers.host + '/' + hash});
         res.end();
       });
     }
