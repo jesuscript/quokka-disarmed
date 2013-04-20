757b8eea3b6020c7c7fb15d729ed50fb5b91218d
KKudryavtsev <konstantin.k.ed@gmail.com>
2013-04-20 11:58:41 +0100

    auth, auth errors

--- client/templates/navbar.js
+++ client/templates/navbar.js
@@ -1,50 +1,122 @@
-(function(){
-    Template.navbar.helpers({
-        username: function(){
-            return Meteor.user().username;
-        },
-        balance: function(){
-            return Meteor.user().balance;
+
+Template.navbar.helpers({
+    username: function(){
+        return Meteor.user().username;
+    },
+    balance: function(){
+        return Meteor.user().balance;
+    }
+});
+
+Template.navbar.events({
+    "click .signup-btn": function(){
+        $("body").append(Meteor.render( Template.signup_dialog ));
+        Session.set("signup_error");
+    },
+    "click .signin-btn": function(){
+        $("body").append(Meteor.render(Template.signin_dialog));
+        Session.set("signin_error");
+    },
+    "click .signout-btn": function(){
+        Meteor.logout();
+    }
+});
+
+
+Template.signin_dialog.rendered = function(){ 
+    $(".auth-dialog input").first().focus();
+    
+};
+
+Template.signup_dialog.rendered = function(){
+    $(".auth-dialog input").first().focus();
+    
+}
+
+Template.signup_dialog.helpers({
+    error: function(){
+        var err = Session.get("signup_error");
+        if(err){
+            if(err.error == 400) return "Empty username";
+            return err.reason;
         }
-    });
+    }
+});
 
-    Template.navbar.events({
-        "click .signup-btn": function(){
-            $("body").append(Meteor.render( Template.signup_dialog ));
-        },
-        "click .signin-btn": function(){
-        }
-    });
+Template.signup_dialog.events({
+    "click .cancel, click .shroud": function(e, tmpl){
+        removeDialog(tmpl,function(){
+            Session.set("signup_error");
+        });
+    },
+    "click .submit-btn": function(e, tmpl){ //TODO keypress Enter submits
+        var password = $("#signup-dialog [name=password]").val();
 
-
-    Template.signup_dialog.rendered = function(){
-        $("#signup-dialog input").first().focus();
-    };
-
-    Template.signup_dialog.events({
-        "click .cancel, click .shroud": function(e, tmpl){
-            removeDialog(tmpl);
-        },
-        "click .submit-btn": function(e, tmpl){ //TODO keypress Enter submits
+        if(password.length < 6){
+            Session.set("signup_error", {
+                error: 406,
+                reason: "Password is too short"
+            });
+        }else{
             Accounts.createUser({
                 username: $("#signup-dialog [name=username]").val(),
-                password: $("#signup-dialog [name=password]").val(),
+                password: password,
                 email: $("#signup-dialog [name=email]").val() //,
-                //secret: "captcure the url"
+                //secret: "captchure the url"
+            },function(err){
+                if(err){
+                    Session.set("signup_error", err);
+                }else{
+                    Session.set("signup_error");
+                    removeDialog(tmpl);
+                }
             });
-            
-            removeDialog(tmpl);
         }
-    });
-
-    function removeDialog(tmpl){
-        var node = tmpl.firstNode;
-
-        $(node).addClass("fade-out");
-        
-        setTimeout(function(){
-            Spark.finalize(node);
-            $(node).remove();
-        }, 500);
     }
-})();
+});
+
+Template.signin_dialog.helpers({
+    error: function(){
+        var err = Session.get("signin_error");
+        if(err){
+            if(err.error ==400) return "Empty username or email";
+            return Session.get("signin_error").reason;
+        }
+    }
+});
+
+Template.signin_dialog.events({
+    "click .cancel, click .shroud": function(e, tmpl){
+        removeDialog(tmpl, function(){
+            Session.set("signin_error");
+        });
+    },
+    "click .submit-btn": function(e, tmpl){
+        var user = $("#signin-dialog [name=user]").val();
+        var password = $("#signin-dialog [name=password]").val();
+        Meteor.loginWithPassword(user, password,function(err){
+            if(err){
+                Session.set("signin_error", err);
+            }else{
+                Session.set("signin_error");
+                removeDialog(tmpl);
+            }
+        });
+    }
+});
+
+
+
+function removeDialog(tmpl, callback){
+    var node = tmpl.firstNode;
+
+    $(node).addClass("fade-out");
+    
+    setTimeout(function(){
+        Spark.finalize(node);
+        $(node).remove();
+
+        callback();
+    }, 500);
+}
+
--- server/auth.js
+++ server/auth.js
@@ -4,8 +4,28 @@ Accounts.onCreateUser(function(options,user){
 });
 
 Accounts.validateNewUser(function(user){
-    //TODO: validate the user
+    if(Meteor.users.find({username: user.username}).count()){
+        throw new Meteor.Error(409, "Username is in use");
+    }
+
+    if(user.username === undefined || user.username.length < 1){
+        throw new Meteor.Error(400, "Empty username");
+    }
+    /*
+    if(user.email && Meteor.users.find({emails: {$elemMatch: {address: user.email}}}).count()){
+        throw new Meteor.Error(412, "Email already exists");
+    }
+      */  
+    if(!validEmail(user.emails[0].address)){
+        throw new Meteor.Error(417, "Inavalid email address")
+    }
+    
     return true;
 });
 
+function validEmail(email){
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return re.test(email);
+}
 
