c80857f25076f2a7369ad89f1720f4c356441d38
Stephan Tual <stephan.tual@gmail.com>
2013-05-09 21:38:45 +0100

    tiny indent modification

--- server/main.js
+++ server/main.js
@@ -7,16 +7,16 @@ var connect = Npm.require('connect');
 __meteor_bootstrap__.app.use(connect.query()).use(function(req, res, next) {
   // check if the url path is a valid sha256 hash 
   if (req.url.length == 65 && req.url.substr(1,64).search(/[a-fA-F0-9]{64}$/) == 0) {
-  next();
+    next();
   } else {
-  // not valid, generate and redirect
-  var crypto = Npm.require('crypto');
-  crypto.randomBytes(512, function(ex, buf) {
-    if (ex) throw ex;
-    var hash = crypto.createHash('sha256').update(buf).digest("hex");
-    res.writeHead(301, {'Location': req.headers['x-forwarded-proto'] + '://' + req.headers.host + '/' + hash});
-    res.end();
-  });
+    // not valid, generate and redirect
+    var crypto = Npm.require('crypto');
+    crypto.randomBytes(512, function(ex, buf) {
+      if (ex) throw ex;
+      var hash = crypto.createHash('sha256').update(buf).digest("hex");
+      res.writeHead(301, {'Location': req.headers['x-forwarded-proto'] + '://' + req.headers.host + '/' + hash});
+      res.end();
+    });
   }
 });
 
