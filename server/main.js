329c0ad95850edcdde911d3548b914671440afdf
Stephan Tual <stephan.tual@gmail.com>
2013-05-09 23:48:21 +0100

    Added support for config file

--- server/main.js
+++ server/main.js
@@ -1,6 +1,3 @@
-// set up SMTP gateway
-process.env.MAIL_URL = 'smtp://johan.daugh%40gmail.com:nueFnRWLFIRZxwx30WxljA@smtp.mandrillapp.com:587';
-
 // triggers page redirect on load, guaranteeing unique token
 var connect = Npm.require('connect');
 
@@ -25,11 +22,7 @@ __meteor_bootstrap__.app.use(connect.query()).use(function(req, res, next) {
 // TODO: currently pointing at testnet connection details
 btcdClient = new BITCOIN.Client({ //none of this is accessible from outside the local machine
   host: 'localhost',
-  port: 18332, 
-  user: 'bitcoinrpc',
-  pass: 'pass'
+  port: process.env.BTCD_PORT, 
+  user: process.env.BTCD_USER,
+  pass: process.env.BTCD_PASS
 });
-
-
-
-
