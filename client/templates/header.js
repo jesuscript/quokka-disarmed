449e2691eaa88cdc5b58caadc3104cce6163bbf4
KKudryavtsev <konstantin.k.ed@gmail.com>
2013-05-11 20:30:44 +0100

    merge

--- client/templates/header.js
+++ client/templates/header.js
@@ -8,6 +8,14 @@ Template.header.helpers({
   },
   message: function() {
     return collections.Flags.findOne({type: 'alert'}).message;
+  },
+  wrongReferrer: function() {
+    var a =  document.createElement('a');
+    a.href = document.referrer;
+    if (a.hostname != 'localhost' && a.hostname != 'www.bittheodds.com') { 
+      return true;
+    }
+    return false;
   }
 });
 
