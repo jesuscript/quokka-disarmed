c6f68fcf4b89f9ae7f5960ed68ecdd6468395e74
KKudryavtsev <konstantin.k.ed@gmail.com>
2013-05-11 20:29:54 +0100

    switched to responsive grid

--- server/methods/betting.js
+++ server/methods/betting.js
@@ -1,31 +1,31 @@
 Meteor.methods({
-    submitBet: function(amount, rangeMin, rangeMax){
-        if(validBet(this.userId, amount, rangeMin, rangeMax)){
-            
-            var gameId = db_helpers.current_game() && db_helpers.current_game()._id;
-            
-            if (gameId === undefined){
-                gameId = collections.Games.insert({
-                    completed: false
-                });
-            }
+  submitBet: function(amount, rangeMin, rangeMax){
+    if(validBet(this.userId, amount, rangeMin, rangeMax)){
+      
+      var gameId = db_helpers.current_game() && db_helpers.current_game()._id;
+      
+      if (gameId === undefined){
+        gameId = collections.Games.insert({
+          completed: false
+        });
+      }
 
-            amount = Math.round(amount * 100000000);
+      amount = Math.round(amount * 100000000);
 
-            collections.Bets.insert({
-                playerId: this.userId,
-                gameId: gameId,
-                amount: amount, 
-                rangeMin: rangeMin,
-                rangeMax: rangeMax
-            });
-        }
+      collections.Bets.insert({
+        playerId: this.userId,
+        gameId: gameId,
+        amount: amount, 
+        rangeMin: rangeMin,
+        rangeMax: rangeMax
+      });
     }
+  }
 });
 
 function validBet(userId, amount, rangeMin, rangeMax){
-    if (!userId) return false;
-    
-    //TODO: check player's balance'
-    return  amount > 0 && typeof rangeMin == "number" && typeof rangeMax == "number";
+  if (!userId) return false;
+  
+  //TODO: check player's balance'
+  return  amount > 0 && typeof rangeMin == "number" && typeof rangeMax == "number";
 }
