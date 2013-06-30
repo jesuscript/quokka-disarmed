c5068053683f5b64ae988c1efa7d1855ab77c213
Stephan Tual <stephan.tual@gmail.com>
2013-07-01 00:47:44 +0100

    - Tested and fixed multiple bugs in bank (now called cashier) - Fixed bug where areDepositConfirmed would trigger 5x7 calls to bitcoind, raping it - Hardened server and client side validation - Fixed issue where large TRX fees could accidentally lead to failed withdrawals - Set form to overall site standards (submit on enter, doesn't erase fields on submit) - Switched withdrawals to only ever use ints internally - Fixed withdrawal dialogs not disappearing following a withdrawal - Fixed withdrawal form fields not getting focus on open - Fixed date format on global wallet balance alert email

--- client/templates/bank/bank.js
+++ client/templates/bank/bank.js
@@ -17,6 +17,9 @@ Template.bank.helpers({
 Template.bank.events({
   "click .bank-btn, click .shroud, click .close-dialog": function(e){
     e.preventDefault();
+    resetWithdrawInterface();
+    if (!Session.get("bank_tmpl_open")) 
+    Session.set("bank_tmpl_mode", "deposit"); // deposit is default view
     Session.set("bank_tmpl_open", !Session.get("bank_tmpl_open"));
   },
   "click .signin-btn": function(e){
@@ -29,12 +32,43 @@ Template.bank.events({
   },
   "click .deposit-btn": function(e){
     e.preventDefault();
+    resetWithdrawInterface();
     Session.set("bank_tmpl_mode", "deposit");
   },
   "click .withdraw-btn": function(e){
     e.preventDefault();
     Session.set("bank_tmpl_mode", "withdraw");
+    calculateWithdrawStatus();
   } 
 });
 
 Template.bank.preserve(["#bank .pull-down", ".shroud"]);
+
+function resetWithdrawInterface() {
+  Session.set("withdraw_error");
+  Session.set("withdraw_bitcoindDown"); // don't show 'btcd down' dialog again as it could have gone back up
+  Session.set("withdraw_tmpl_transfer"); // don't show 'transfer in progress' dialog again
+}
+
+
+function calculateWithdrawStatus(){
+  if (Meteor.user().balance) {
+    Meteor.call('areDepositsConfirmed', function(err, depositsConfirmed) {
+      if (err) {
+        Session.set("withdraw_bitcoindDown", {
+          error: err.error,
+          reason: err.reason
+        });
+      }
+      Session.set('depositsConfirmed', depositsConfirmed);
+    }); 
+    Meteor.call('getOutstandingDeposits', function(err, outstandingDeposits) {
+      if (err) console.log(err);
+      Session.set('outstandingDeposits', outstandingDeposits);
+    }); 
+    Meteor.call('getTimeToValidateDeposits', function(err, timeToValidateDeposits) {
+      if (err) console.log(err);
+      Session.set('timeToValidateDeposits', timeToValidateDeposits);
+    });
+  }
+};
