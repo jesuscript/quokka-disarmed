7e8530939a24add92e5089ac4f755d454b06ab0d
KKudryavtsev <konstantin.k.ed@gmail.com>
2013-07-05 11:03:12 +0100

    inverted slider colors

--- client/templates/last_lucky_numbers.js
+++ client/templates/last_lucky_numbers.js
@@ -1,7 +1,8 @@
 Template.lastLuckyNumbers.helpers({
   luckyNumbers: function(){
     var games = Collections.Games.find({completed: true},{sort: {completedAt: -1}}).fetch();
+    var nums = _.map(games, function(game){ return game.luckyNum; });
 
-    return _.map(games, function(game){ return game.luckyNum; });
+    return nums;
   }
 });
