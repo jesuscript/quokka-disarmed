7752b695d1dc595386c4f03575842e00f321a253
Stephan Tual <stephan.tual@gmail.com>
2013-07-07 19:55:55 +0100

    First go at a stacked chart, this file is for information only

--- client/widgets/stacked_bet_graph.js
+++ client/widgets/stacked_bet_graph.js
@@ -1,184 +1,93 @@
 $.widget("bto.stackedBetGraph",$.bto.betGraph,{
   
-  _bets: [],
-  valueArray: [],
-  rootContainer: '',
+  _create: function(){
+    console.log('calling create');
+    this._super();
+
+
+    var histcatexpshort = [ 
+      { 
+        "key" : "Johan Daugh" , 
+        "values" : [ [ 1 , 20] , [ 2 , 20] , [ 3 , 20] ] 
+      } , 
+      { 
+        "key" : "bob ddd" , 
+        "values" : [ [ 1 , 10] , [ 2 , 10] , [ 3 , 10] ] 
+      } , 
+      { 
+        "key" : "Smith joen" , 
+        "values" : [ [ 1 , 5] , [ 2 , 5] , [ 3 , 5] ] 
+      }
+    ];   
+
+    nv.addGraph(function() {
+      var chart = nv.models.stackedAreaChart()
+                    .x(function(d) { return d[0] })
+                    .y(function(d) { return d[1] })
+                    .clipEdge(true);
+
+
+      chart.xAxis
+          .showMaxMin(false);
+
+
+      chart.yAxis
+          .tickFormat(d3.format(',.2f'));
+
+        d3.select('.bet-graph svg')
+          .datum(histcatexpshort)
+            .transition().duration(500).call(chart);
+
+      nv.utils.windowResize(chart.update);
+
+      return chart;
+    });
+
+
+
+  },
+
 
   bets: function(bets){
     if(bets){
       this._bets = bets;
-      this.recalculateBets();
+      this._convertBetsToStackData()
       this.redraw();
     }
     return bets;
   },
 
 
-  _create: function(){
-    this._super();
-    $(window).resize(_.debounce(this.draw.bind(this), 5));
-    this.draw();
-  },
-
-
-  draw: function() {
-    console.log('draw invoked');
-    this.element.find("svg").remove();
-    this._svg = d3.select(this.element[0]).append("svg");
-
-    var margin = {top: 40, right: 15, bottom: 30, left: 25};
-    var targetWidth = this.element.width();
-    var svgWidth = targetWidth;
-    var svgHeight = 120;
-    var barWidth = targetWidth/140;
-    var titleShift = 20;
-    var chartWidth = svgWidth - margin.left - margin.right - titleShift;
-    var chartHeight = svgHeight - margin.top - margin.bottom;
-    
-    var x = d3.scale.linear()
-      .domain([1, 100])
-      .range([0, chartWidth]);
-
-    var y = d3.scale.linear()
-      .range([chartHeight, 0]);
-
-    y.domain([0, d3.max(this.valueArray, function(d) { return d.total; })]);
-
-    function getXTickValues() {
-      var modulo;
-      if (chartWidth > 1600) {
-        modulo = 1;
-      } else if (chartWidth > 400) {
-        modulo = 5;
-      } else {
-        modulo = 10;
+  _convertBetsToStackData:function(){
+    var convertedBets = [];
+    for (var i = this._bets.length - 1; i >= 0; i--) {
+      var userBets = [];
+      for (var num = 1; num<=100; num++) {
+        if (num >= this._bets[i].rangeMin && num <= this._bets[i].rangeMax) {
+          var rangeSize = this._bets[i].rangeMax - this._bets[i].rangeMin;
+          userBets.push( [num, intToBtc(this._bets[i].amount/rangeSize)] );
+        } else {
+          userBets.push( [num, 0] );
+        }
       }
-      var theArray = [1];
-      for (var i = 1; i <= 100; i++) {
-        if (i % modulo === 0  ) theArray.push(i);
-      };
-      return theArray;
-    }
-
-    var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom")
-      .tickValues(getXTickValues());
-
-    var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left")
-      .ticks(3);
-
-    this._svg.selectAll("g").remove();
-    this._svg.selectAll(".bar").remove();
-    
-    this.rootContainer = this._svg.attr("width", svgWidth)
-      .attr("height", svgHeight)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    this.rootContainer.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(" + titleShift + "," + chartHeight + ")")
-      .call(xAxis);
-
-    this.rootContainer.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("BTC");
-
-    this.rootContainer.selectAll(".bar")
-      .data(this.valueArray)
-      .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.num); })
-      .attr("width", barWidth)
-      .attr("transform", "translate(" + (titleShift - (barWidth/2)) + ",0)")
-      .attr("y", function(d) { return y(d.total); })
-      .attr("height", function(d) { return chartHeight - y(d.total); })
+      convertedBets.push( {"key": this._bets[i].playerId, "values": userBets} );
+    };
+    this._nvd3Data = convertedBets;
   },
+
+
+  // draw: function() {
+    
+  // },
 
 
   redraw: function() {
-    console.log('redraw invoked');
-    var margin = {top: 40, right: 15, bottom: 30, left: 25};
-    var targetWidth = this.element.width();
-    var svgWidth = targetWidth;
-    var svgHeight = 120;
-    var barWidth = targetWidth/140;
-    var titleShift = 20;
-    var chartWidth = svgWidth - margin.left - margin.right - titleShift;
-    var chartHeight = svgHeight - margin.top - margin.bottom;
 
-    var y = d3.scale.linear()
-        .range([chartHeight, 0]);
-
-    y.domain([0, d3.max(this.valueArray, function(d) { return d.total; })]);
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left")
-        .ticks(3);
-
-    this.rootContainer.select(".y.axis").transition().call(yAxis);
-
-    this.rootContainer.selectAll(".bar")
-        .data(this.valueArray)
-        .transition()
-        .duration(500)
-        .attr("y", function(d) { return y(d.total); })
-        .attr("height", function(d) { return chartHeight - y(d.total); })
+       d3.select('.bet-graph svg')
+          .datum(this._nvd3Data)
+            .transition().duration(500).call(chart);
   },
 
-
-  bet: function() {
-    var rAmount1 = this.getRandomInt(1, 100);
-    var rAmount2 = this.getRandomInt(rAmount1, 100);
-    this._bets.push({"username": this.generateRandomName(), "num1": rAmount1, "num2": rAmount2});
-    this.recalculateBets();
-    this.redraw();
-  },
-
-
-  revoke: function() {
-    this._bets.splice(Math.floor(Math.random() * this.bets.length), 1);
-    this.recalculateBets();
-    this.redraw();
-  },
-
-
-  recalculateBets: function() {
-    var convertedBets = [];
-
-    for (var i = this._bets.length - 1; i >= 0; i--) {
-      for (var j = this._bets[i].rangeMax; j >= this._bets[i].rangeMin; j--) {
-        var rangeSize = this._bets[i].rangeMax - this._bets[i].rangeMin;
-        if (rangeSize == 0) rangeSize = 1;
-        convertedBets.push( {"amount": this._bets[i].amount/rangeSize/100000000, "num": j} );
-      }
-    };
-
-    this.valueArray = [];
-  
-    for (var i = 1; i<=100; i++) {
-      var total = 0;
-      for (var j = convertedBets.length - 1; j >= 0; j--) {
-        if (convertedBets[j].num == i) total = total + convertedBets[j].amount;
-      };
-      this.valueArray.push( {"num": i, "total": total} );
-    }
-  },
-
-
-  getRandomInt: function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
 
 });
 
