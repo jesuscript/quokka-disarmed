8b2350e430e5c971a65f4f8644e87113934885f7
Stephan Tual <stephan.tual@gmail.com>
2013-07-16 22:52:14 +0100

    Fixed horrible loading order bug due to me using Meteor.startup() where I shouldn't have

--- server/lib/config.js
+++ server/lib/config.js
@@ -1,33 +1,31 @@
-Meteor.startup(function(){
-  var btcdEnv = process.env.BTCD_ENV || 'testnet';
-  process.env.TRX_FEE = 10000;
+var btcdEnv = process.env.BTCD_ENV || 'testnet';
+process.env.TRX_FEE = 10000;
 
-  switch (process.env.NODE_ENV) {
-  case 'development':
-    process.env.PROTO = 'http';
-    process.env.MAIL_URL = ''; // leave blank so meteor dumps the mail to console
-    if (btcdEnv == 'testnet') {
-      process.env.BTCD_PORT = '18332';
-      process.env.BTCD_USER = 'bitcoinrpc';
-      process.env.BTCD_PASS = 'pass';
-    } else {
-      process.env.BTCD_PORT = '8332';
-      process.env.BTCD_USER = 'bitcoinrpc';
-      process.env.BTCD_PASS = '3441KQjAyahZueZAP44KNHPNbFDfXLumJiaxeFXCZU1D';
-    }
-    break;
-  case 'production':
-    process.env.PROTO = 'https';
-    process.env.MAIL_URL = 'smtp://johan.daugh%40gmail.com:nueFnRWLFIRZxwx30WxljA@smtp.mandrillapp.com:587';
-    if (btcdEnv == 'testnet') {
-      process.env.BTCD_PORT = '18332';
-      process.env.BTCD_USER = 'bto_bitcoin_RPC';
-      process.env.BTCD_PASS = '9mxP79uTEgzctr4ZYVESK3499pAXGrLibqHZud7BdwFU';
-    } else {
-      process.env.BTCD_PORT = '8332';
-      process.env.BTCD_USER = 'bto_bitcoin_RPC';
-      process.env.BTCD_PASS = '9mxP79uTEgzctr4ZYVESK3499pAXGrLibqHZud7BdwFU';
-    }
-    break;
+switch (process.env.NODE_ENV) {
+case 'development':
+  process.env.PROTO = 'http';
+  process.env.MAIL_URL = ''; // leave blank so meteor dumps the mail to console
+  if (btcdEnv == 'testnet') {
+    process.env.BTCD_PORT = '18332';
+    process.env.BTCD_USER = 'bitcoinrpc';
+    process.env.BTCD_PASS = 'pass';
+  } else {
+    process.env.BTCD_PORT = '8332';
+    process.env.BTCD_USER = 'bitcoinrpc';
+    process.env.BTCD_PASS = '3441KQjAyahZueZAP44KNHPNbFDfXLumJiaxeFXCZU1D';
   }
-});
+  break;
+case 'production':
+  process.env.PROTO = 'https';
+  process.env.MAIL_URL = 'smtp://johan.daugh%40gmail.com:nueFnRWLFIRZxwx30WxljA@smtp.mandrillapp.com:587';
+  if (btcdEnv == 'testnet') {
+    process.env.BTCD_PORT = '18332';
+    process.env.BTCD_USER = 'bto_bitcoin_RPC';
+    process.env.BTCD_PASS = '9mxP79uTEgzctr4ZYVESK3499pAXGrLibqHZud7BdwFU';
+  } else {
+    process.env.BTCD_PORT = '8332';
+    process.env.BTCD_USER = 'bto_bitcoin_RPC';
+    process.env.BTCD_PASS = '9mxP79uTEgzctr4ZYVESK3499pAXGrLibqHZud7BdwFU';
+  }
+  break;
+}
