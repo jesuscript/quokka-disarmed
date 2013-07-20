fc41191442f9fe22d8cb04577328ffa3571cc231
Stephan Tual <stephan.tual@gmail.com>
2013-07-20 16:18:12 +0100

    Temporarily removed debug info

--- server/db/debug.js
+++ server/db/debug.js
@@ -1,30 +1,30 @@
-if (process.env.SHOW_DEBUG) {
+// if (process.env.SHOW_DEBUG) {
 
-  var wrappedFind = Meteor.Collection.prototype.find;
+//   var wrappedFind = Meteor.Collection.prototype.find;
 
-  Meteor.Collection.prototype.find = function () {
-    var cursor = wrappedFind.apply(this, arguments);
-    var collectionName = this._name;
+//   Meteor.Collection.prototype.find = function () {
+//     var cursor = wrappedFind.apply(this, arguments);
+//     var collectionName = this._name;
 
-    cursor.observeChanges({
-      added: function (id, fields) {
-        console.log(collectionName, 'added', id, fields);
-      },
+//     cursor.observeChanges({
+//       added: function (id, fields) {
+//         console.log(collectionName, 'added', id, fields);
+//       },
 
-      changed: function (id, fields) {
-        console.log(collectionName, 'changed', id, fields);
-      },
+//       changed: function (id, fields) {
+//         console.log(collectionName, 'changed', id, fields);
+//       },
 
-      movedBefore: function (id, before) {
-        console.log(collectionName, 'movedBefore', id, before);
-      },
+//       movedBefore: function (id, before) {
+//         console.log(collectionName, 'movedBefore', id, before);
+//       },
 
-      removed: function (id) {
-        console.log(collectionName, 'removed', id);
-      }
-    });
+//       removed: function (id) {
+//         console.log(collectionName, 'removed', id);
+//       }
+//     });
 
-    return cursor;
-  };
+//     return cursor;
+//   };
 
-}
+// }
