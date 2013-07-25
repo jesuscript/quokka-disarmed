9ad568a0bc99626c7e2eb28f8132d71b56be0933
Stephan Tual <stephan.tual@gmail.com>
2013-07-25 23:08:29 +0100

    Completely useless , but highly pleasing to my mind's eye, reordering of LOCs.

--- client/subscribe.js
+++ client/subscribe.js
@@ -1,22 +1,22 @@
 Meteor.startup(function(){
-  Meteor.subscribe("games");
-  Meteor.subscribe("flags");
-  Meteor.subscribe("userData");
-  Meteor.subscribe("users");
-  Meteor.subscribe("payouts");
-  Meteor.subscribe("news");
-  Meteor.subscribe("allTimeStats");
-  Meteor.subscribe("connections");
-  Meteor.subscribe("connectionsMonitor");
-  Meteor.subscribe("chatMsgs");
   Meteor.subscribe("activity");
+  Meteor.subscribe("allTimeStats");
 
   var betsHandle;
-  
   Deps.autorun(function(){
     betsHandle && betsHandle.stop();
-    
     Collections.Games.find({completed: false}).fetch(); // for reactivity
     betsHandle = Meteor.subscribe("bets");  
   });
+
+  Meteor.subscribe("chatMsgs");
+  Meteor.subscribe("connections");
+  Meteor.subscribe("connectionsMonitor");
+  Meteor.subscribe("flags");
+  Meteor.subscribe("games");
+  Meteor.subscribe("news");
+  Meteor.subscribe("payouts");
+  Meteor.subscribe("userData");
+  Meteor.subscribe("users");
 });
+
--- server/db/publish/publish.js
+++ server/db/publish/publish.js
@@ -1,31 +1,40 @@
+Meteor.publish("activity", function(){
+  return Collections.Activity.find({},{sort: {timestamp: -1}, limit: 30});
+});
+
 AddressPool = new Meteor.Collection('addressPool'); // server only
 
-Meteor.publish('flags', function(){
-  return Collections.Flags.find({});
-});
-
-Meteor.publish('news', function(){
-  return Collections.News.find({}, {sort: {timestamp: -1}, limit: 10});
-});
-
-Meteor.publish('games', function(){
-  return Collections.Games.find({},{sort: {createdAt: -1}, limit: 100});
+Meteor.publish('allTimeStats', function(){
+  return Collections.AllTimeStats.find({});
 });
 
 Meteor.publish('bets', function(){ //update to publish only for the current game
   return Collections.Bets.find({gameId: DB.currentGame()._id});
 });
 
-Meteor.publish('userData', function(){  // built-in meteor collection
-  return Meteor.users.find({_id: this.userId}, 
-                           {fields: { 
-                             'balance': 1,
-                             'depositAddress': 1
-                           }});
+Meteor.publish("chatMsgs", function(){
+  return Collections.ChatMsgs.find({},{sort: {timestamp: -1}, limit: 30});
 });
 
-// the user shouldn't have the right to modify anything within their user profiles
-Meteor.users.deny({update: function () { return true; }});
+Meteor.publish('connections', function(){
+  return Collections.Connections.find({});
+});
+
+Meteor.publish('flags', function(){
+  return Collections.Flags.find({});
+});
+
+Meteor.publish('games', function(){
+  return Collections.Games.find({},{sort: {createdAt: -1}, limit: 100});
+});
+
+Meteor.publish('news', function(){
+  return Collections.News.find({}, {sort: {timestamp: -1}, limit: 10});
+});
+
+Meteor.publish('payouts', function(){
+  return Collections.Payouts.find({},{sort: {timestamp: -1}, limit: 10});
+});
 
 Meteor.publish('users', function(){ // TODO - this publishes the entire list of usernames to the client, could grow to gigs!
   return Meteor.users.find({}, {
@@ -35,24 +44,22 @@ Meteor.publish('users', function(){ // TODO - this publishes the entire list of 
   });
 });
 
-Meteor.publish('payouts', function(){
-  return Collections.Payouts.find({},{sort: {timestamp: -1}, limit: 10});
+// the user shouldn't have the right to modify anything within their user profiles
+Meteor.users.deny({update: function () { return true; }});
+
+Meteor.publish('userData', function(){  // built-in meteor collection
+  return Meteor.users.find({_id: this.userId}, 
+                           {fields: { 
+                             'balance': 1,
+                             'depositAddress': 1
+                           }});
 });
 
-Meteor.publish('allTimeStats', function(){
-  return Collections.AllTimeStats.find({});
-});
 
-Meteor.publish('connections', function(){
-  return Collections.Connections.find({});
-});
 
-Meteor.publish("chatMsgs", function(){
-  return Collections.ChatMsgs.find({},{sort: {timestamp: -1}, limit: 30});
-});
 
-Meteor.publish("activity", function(){
-  return Collections.Activity.find({},{sort: {timestamp: -1}, limit: 30});
-});
+
+
+
 
 
