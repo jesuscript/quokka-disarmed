11051344069590f8fd34c2ecf1278c6fd6995ae6
Stephan Tual <stephan.tual@gmail.com>
2013-07-27 11:13:43 +0100

    Leveraging meteor api to pull username serverside instead of using findone()

--- server/methods/chat.js
+++ server/methods/chat.js
@@ -2,7 +2,7 @@ Meteor.methods({
   submitChatMsg: function(msg){
     Collections.ChatMsgs.insert({
       userId: this.userId,
-      username: this.userId ? Meteor.users.findOne({_id: this.userId}).username : "Anonymous",
+      username: this.userId ? Meteor.user().username : "Anonymous",
       message: msg,
       timestamp: (new Date()).getTime()
     });
