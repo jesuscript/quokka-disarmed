5bcf56a680beffa04a196d0e24112af5a08141f6
Stephan Tual <stephan.tual@gmail.com>
2013-07-30 00:38:53 +0100

    Prevents subsequent calls of qrcode generator if it already has been generated

--- client/templates/bank/bank_deposit.js
+++ client/templates/bank/bank_deposit.js
@@ -1,11 +1,13 @@
 Template.bankDeposit.helpers({
   qrCode: function(){
-    Meteor.call('getQrImg', function(err, qrCode) {
-      if (err) console.error(err);
-      Session.set('qrCodeImg', qrCode);
-    }); 
-    if (Session.get('qrCodeImg'))
+    if (Session.get('qrCodeImg')) {
       return Session.get('qrCodeImg');
+    } else {
+      Meteor.call('getQrImg', function(err, qrCode) {
+        if (err) console.error(err);
+        Session.set('qrCodeImg', qrCode); // contains the img tag
+      });
+    }
   },
   depositAddress: function(){
     return Meteor.user() && Meteor.user().depositAddress;
